b64ff3dfe69fd76511e3cdb615ccfebed3f6dc3a
Heikki Seppanen <6179953+heikkiseppanen@users.noreply.github.com>
2023-07-11 12:55:15 +0300

    Testing ray hit and colorization

--- src/app/app.c
+++ src/app/app.c
@@ -6,7 +6,7 @@
 /*   By: hseppane <marvin@42.ft>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 12:09:03 by hseppane          #+#    #+#             */
-/*   Updated: 2023/07/10 14:00:53 by hseppane         ###   ########.fr       */
+/*   Updated: 2023/07/11 12:35:30 by hseppane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,10 @@ int	app_terminate(t_app *instance, int exit_code)
 }
 
 #define ARGB_RED 0x00FF0000
-#define ARGB_GREEN 0x00FF0000
+#define ARGB_GREEN 0x0000FF00
 #define ARGB_BLUE 0x000000FF
 #define ARGB_NORD_RED 0x00BF616A
+#define ARGB_NORD_GREEN 0x00A3BE8C
 #define ARGB_NORD_BLUE 0x002E3440
 
 typedef unsigned int t_argb32;
@@ -58,6 +59,12 @@ typedef struct s_ray
 	t_float3 direction;
 } t_ray;
 
+typedef struct s_ray_hit
+{
+	t_float3 position;
+	t_float3 normal;
+} t_ray_hit;
+
 //t_argb32	ray_cast(t_ray *ray, jj
 
 float	ray_sphere_intersect(const t_ray *ray, t_float3 center, float radius)
@@ -66,17 +73,15 @@ float	ray_sphere_intersect(const t_ray *ray, t_float3 center, float radius)
 	const float a = ft_float3_dot(ray->direction, ray->direction);
 	const float b = ft_float3_dot(oc, ray->direction);
 	const float c = ft_float3_dot(oc, oc) - (radius * radius);
-	float f;
+	float d;
 
-	f = (b * b) - (a * c);
-	if (f < 0)
+	d = (b * b) - (a * c);
+	if (d < 0)
 	{
 		return -1.0f;
 	}
-	d = 
-	{
-		return ((-b - sqrtf(d)) / a);
-	}
+	d = sqrtf(d);
+	return ((-b - d) / a);
 }
 
 void framebuf_put_pixel(t_framebuf *output, t_float3 position, t_argb32 color)
@@ -100,29 +105,15 @@ int	app_loop(t_app *app)
 	/* Draw */
 
 	//float fov = M_PI;
-	t_float3 pos[] = {
-		{-0.5f, 0.0f, -1.0f},
-		{0.5f, 0.0f, -1.0f},
+	static t_float3 pos[] = {
+		{-0.25f, 0.0f, 0.0f},
+		{0.25f, 0.0f, 0.0f},
+		{0.0f, -0.25f, 0.0f},
 	};
 	float rad = 0.5f;
 	t_framebuf *out = &window->framebuffer;
-	//const float aspect = (float)out->width / (float)out->height;
+	const float aspect = (float)out->width / (float)out->height;
 
-
-	//while (y < out->height)
-	//{
-	//	if (y & 1)
-	//	{
-	//		int x = 0;
-
-	//		while (x < out->width)
-	//		{
-	//			framebuf_put_pixel(out, (t_float3){x, y, 0.0f}, ARGB_RED);
-	//			++x;
-	//		}
-	//	}
-	//	++y;
-	//}
 	int y = 0;
 	while (y < out->height)
 	{
@@ -132,21 +123,32 @@ int	app_loop(t_app *app)
 			t_float3 p;
 			p.x = ((float)x + 0.5f) / (float)out->width * 2 - 1;
 			p.y = ((float)y + 0.5f) / (float)out->height * 2 - 1;
+			p.x *= aspect;
 			p.z = -1.0f;
 			p = ft_float3_normalize(p);
 
-			t_ray ray = {{}, p};
+			t_ray ray = {{0,0,2.0f}, p};
+			
+			float mul = 500.0f;
+			t_argb32 col = 0;
+			for (int i = 0; i < 3; ++i)
+			{
+				float m = ray_sphere_intersect(&ray, pos[i], rad);
+				if (m > 0 && m <= mul)
+				{
+					mul = m;
+					t_float3 hit = ft_float3_scalar(ray.direction, m);
+					hit = ft_float3_add(ray.origin, hit); 
+					hit = ft_float3_sub(hit, pos[i]);
+					hit = ft_float3_normalize(hit);
+					hit.x = (hit.x + 1) * 0.5f;
+					hit.y = (hit.y + 1) * 0.5f;
+					hit.z = (hit.z + 1) * 0.5f;
+					col = (int)(hit.x * 255.0f) << 16 | (int)(hit.y * 255.0f) << 8 | (int)(hit.z * 255.0f);
+				}
+			}
 
-			if (ray_sphere_intersect(&ray, pos, rad) < 0)
-			{
-				ft_printf("put nord %i %i\n", x, y);
-				framebuf_put_pixel(out, (t_float3){x, y, 0.0f}, ARGB_NORD_BLUE);
-			}
-			else 
-			{
-				ft_printf("put red %i %i\n", x, y);
-				framebuf_put_pixel(out, (t_float3){x, y, 0.0f}, ARGB_NORD_RED);
-			}
+			framebuf_put_pixel(out, (t_float3){x, out->height - y, 0.0f}, col);
 
 			++x;
 		}
