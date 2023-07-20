fda4458b19824c296e9f55bdafe348b9303d704f
Heikki Seppanen <6179953+heikkiseppanen@users.noreply.github.com>
2023-07-20 12:39:46 +0300

    Added basic types for boolean/error handling

---
+++ src/minirt.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseppane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 14:20:38 by hseppane          #+#    #+#             */
+/*   Updated: 2023/07/20 12:37:41 by hseppane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+typedef char t_bool;
+
+# define RT_TRUE 1
+# define RT_FALSE 0
+
+typedef char t_err;
+
+# define RT_SUCCESS 1,
+# define RT_FAILURE 0,
+
+#endif
