f54a1e60c1ad5c9ba117549ffe46aa06093ae42c
Heikki Seppanen <6179953+heikkiseppanen@users.noreply.github.com>
2023-07-21 14:55:39 +0300

    Rudimentary ecs implementation

---
+++ src/scene/ecs.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ecs.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseppane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 12:52:41 by hseppane          #+#    #+#             */
+/*   Updated: 2023/07/21 14:26:27 by hseppane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "scene/ecs.h"
+
+#include <ft/math.h>
+
+static size_t	get_component_size(t_ecs_type type)
+{
+	static size_t	size_table[] = {
+		sizeof(t_ecs_list),
+		sizeof(t_float3),
+		sizeof(t_float3),
+	};
+
+	return (size_table[type]);
+}
+
+t_err	ecs_init(t_ecs *e)
+{
+	t_ecs_type type;
+
+	*e = (t_ecs){};
+	type = 0;
+	while (type < ECS_CAMERA)
+	{
+		if (!ft_buf_init(&e->components[type], 1, get_component_size(type)))
+		{
+			ecs_del(e);
+			return (RT_FAILURE);
+		};
+		++type;
+	}
+	return (RT_SUCCESS);
+}
+
+void	ecs_del(t_ecs *e)
+{
+	t_ecs_type	type;
+
+	type = 0;
+	while (type < ECS_TYPE_COUNT)
+	{
+		ft_buf_del(e->components + type);
+		++type;
+	}
+}
+
+t_id	ecs_entity_create(t_ecs *e)
+{
+	static t_ecs_list	empty_list = {};
+
+	if (!ft_buf_pushback(&e->components[ECS_LIST], &empty_list, 1))
+	{
+		return (0);
+	}
+	return ++e->accumulator;
+}
+
+t_err	ecs_add_component(t_ecs* e, t_id entity, void *data, t_ecs_type type)
+{
+	t_ecs_list *list;
+
+	list = ft_buf_get(&e->components[ECS_LIST], entity - 1);
+	if (list->reference[type] != 0
+		|| !ft_buf_pushback(&e->components[type], data, 1))
+	{
+		return (RT_FAILURE);
+	}
+	list->reference[type] = e->components[type].size;
+	return (RT_SUCCESS);
+}
+
+void	*ecs_get_component(const t_ecs* e, t_id entity, t_ecs_type type)
+{
+	t_ecs_list	*list;
+	t_id		id;
+
+	list = ft_buf_get(&e->components[ECS_LIST], entity - 1);
+	id = list->reference[type];
+	if (!id)
+	{
+		return (NULL);
+	}
+	return (ft_buf_get(&e->components[type], id - 1));
+}
---
+++ src/scene/ecs.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ecs.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseppane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 11:51:25 by hseppane          #+#    #+#             */
+/*   Updated: 2023/07/21 13:25:49 by hseppane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ECS_H
+# define ECS_H
+
+# include "minirt.h"
+
+# include <ft/buf.h>
+
+typedef unsigned int t_id;
+
+typedef enum e_ecs_type
+{
+	ECS_LIST = 0,
+	ECS_POSITION,
+	ECS_ROTATION,
+	ECS_CAMERA,
+	ECS_LIGHT,
+	ECS_GEOMETRY,
+	ECS_TYPE_COUNT
+}	t_ecs_type;
+
+typedef struct s_ecs_list
+{
+	t_id	reference[ECS_TYPE_COUNT];
+}	t_ecs_list;
+
+typedef struct s_ecs
+{
+	t_id	accumulator;
+	t_buf	components[ECS_TYPE_COUNT];
+}	t_ecs;
+
+t_err	ecs_init(t_ecs *e);
+void	ecs_del(t_ecs *e);
+
+// Creates a new empty entity. Returns 0 on failure.
+t_id	ecs_entity_create(t_ecs* e);
+
+// Adds a new component for the provided entity.
+t_err	ecs_add_component(t_ecs* e, t_id entity, void *data, t_ecs_type type);
+
+// Returns a reference to a component of an entity or NULL if none.
+void	*ecs_get_component(const t_ecs* e, t_id entity, t_ecs_type type);
+
+#endif
